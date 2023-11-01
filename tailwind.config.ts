64e8c99ccfc604444a3af4c5aeed8e5462fcfb2c
Chris <36116606+TophC7@users.noreply.github.com>
2023-11-01 02:00:11 -0400

    More nice defaults

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,65 +4,49 @@ import type { Config } from 'tailwindcss';
 
 /** @type {import('tailwindcss').Config}*/
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				Inter: ['Inter', 'Satoshi', 'Alpino', 'sans-serif'],
-				Satoshi: ['Satoshi', 'Inter', 'Alpino', 'sans-seri'],
-				Alpino: ['Alpino', 'Inter', 'Satoshi', 'sans-seri']
-			}
-		}
-	},
-	daisyui: {
-		themes: [
-			{
-				dark: {
-					primary: '#66CC8A',
-
-					secondary: '#377CFB',
-
-					accent: '#ff3e00',
-
-					neutral: '#FFFFFF',
-
-					'base-100': '#333C4D',
-
-					info: '#3ABFF8',
-
-					success: '#36D399',
-
-					warning: '#FBBD23',
-
-					error: '#F87272'
-				},
-				light: {
-					primary: '#66CC8A',
-
-					secondary: '#377CFB',
-
-					accent: '#ff3e00',
-
-					neutral: '#333C4D',
-
-					'base-100': '#FFFFFF',
-
-					info: '#3ABFF8',
-
-					success: '#36D399',
-
-					warning: '#FBBD23',
-
-					error: '#F87272'
-				}
-			},
-			'cupcake',
-			'retro',
-			'business',
-			'corporate'
-		]
-	},
-	plugins: [forms, daisyui]
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ['Inter', 'Satoshi', 'Alpino', 'sans-serif'],
+        Satoshi: ['Satoshi', 'Inter', 'Alpino', 'sans-seri'],
+        Alpino: ['Alpino', 'Inter', 'Satoshi', 'sans-seri']
+      }
+    }
+  },
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          primary: '#ff3e00',
+          secondary: '#3b82f6',
+          accent: '#fde047',
+          neutral: '#ffffff',
+          'base-100': '#001820',
+          info: '#2dd4bf',
+          success: '#4ade80',
+          warning: '#fbbd23',
+          error: '#fb7185'
+        },
+        light: {
+          primary: '#ff3e00',
+          secondary: '#3b82f6',
+          accent: '#fde047',
+          neutral: '#001820',
+          'base-100': '#ffffff',
+          info: '#2dd4bf',
+          success: '#4ade80',
+          warning: '#fbbd23',
+          error: '#fb7185'
+        }
+      },
+      'cupcake',
+      'retro',
+      'business',
+      'corporate'
+    ]
+  },
+  plugins: [forms, daisyui]
 } satisfies Config;
 
 export default config;
